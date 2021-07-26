1a16e3bb979051d1a04a6c3b363eed952d9fdf02
Nodir <nodirmalikov2004@yandex.ru>
2021-07-26 22:40:23 +0500

    first commit

--- tgbot/services/funcs.py
+++ tgbot/services/funcs.py
@@ -20,7 +20,7 @@ async def youtube_download(link: str):
             logging.info('Tayyor!')
 
     ydl_opts = {
-        'format': '[filesize<100M]',
+        'format': '[filesize<50M]',
         'progress_hook': [progress_hook],
         'outtmpl': f'{config.tg_bot.yt_path}%(extractor)s-%(id)s-%(title)s.%(ext)s',
     }
